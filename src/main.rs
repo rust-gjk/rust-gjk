6673f6e1e59c9a9c72381dd5d6dcb8a46ad7a204
Lukáš Hozda <luk.hozda@gmail.com>
2018-11-11 13:25:08 +0100

    kalulačka

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,30 @@ extern crate rstd;
 use rstd::prelude::*;
 
 fn main() {
-	let jmeno: String = prompt("zadej svoje jméno");
-	println!("tvoje jméno je {:?}", jmeno.split_nonempty('a'));
+	let mut vysledek: i64 = 0;	
+
+	loop {
+		let vstup: String = read();
+		let seznam = vstup.split_nonempty(' ');
+
+		match seznam[0].as_str() {
+			"+" => {
+				vysledek += seznam[1].parse::<i64>().unwrap();
+				println!("{}", vysledek);
+			},
+			"-" => {
+				vysledek += seznam[1].parse::<i64>().unwrap();
+				println!("{}", vysledek);
+			},
+			"*" => {
+				vysledek += seznam[1].parse::<i64>().unwrap();
+				println!("{}", vysledek);
+			},
+			"/" => {
+				vysledek += seznam[1].parse::<i64>().unwrap();
+				println!("{}", vysledek);
+			},
+			x => println!("neznamy"),
+		}
+	}
 }
